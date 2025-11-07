7a9694694b14f2cb4402b5e10313bb64c60c1eb2
Mizuto Segawa <192568839+mimimichan456@users.noreply.github.com>
2025-11-07 20:30:30 +0900

    #9 qgis_env.pyを新規作成し、QGISセッションの管理機能を追加

---
+++ scripts/QGIS/qgis_env.py
@@ -0,0 +1,18 @@
+# qgis_env.py
+import sys, os
+from qgis.core import QgsApplication
+
+class QgisSession:
+    def __enter__(self):
+        sys.path.append("/Applications/QGIS.app/Contents/Resources/python")
+        os.environ["PROJ_LIB"] = "/Applications/QGIS.app/Contents/Resources/proj"
+
+        QgsApplication.setPrefixPath("/Applications/QGIS.app/Contents/MacOS", True)
+        self.qgs = QgsApplication([], False)
+        self.qgs.initQgis()
+        print("✅ QGIS started")
+        return self.qgs
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.qgs.exitQgis()
+        print("🛑 QGIS closed")
